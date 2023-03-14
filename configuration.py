ea1fdbf8c6f7aa0f83ed10d25ed42776d80da269
Danara <dzhukaeva07@yandex.ru>
2023-03-14 13:50:24 +0300

    first commit

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = "  https://32cdf196-8b6e-42d7-b6ab-61508063ae3e.serverhub.praktikum-services.ru"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
